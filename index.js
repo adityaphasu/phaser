38c1b0b904e6a62ba14222ffaa2feb1d31344b5b
Aditya Phasu <adityaphasu919@gmail.com>
2025-09-15 23:09:09 +0530

    Add foxgirl sprite and implement movement states

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-let player;
+let foxGirl;
 let cursors;
+let speed = 3;
+let currentState;
 
 const config = {
   type: Phaser.AUTO,
@@ -14,40 +16,165 @@ const config = {
 };
 const game = new Phaser.Game(config);
 
+const states = {
+  idle: {
+    onEnter() {
+      foxGirl.anims.stop();
+    },
+    onUpdate() {
+      if (cursors.shift.isDown) {
+        if (cursors.left.isDown) return "run-left";
+        if (cursors.right.isDown) return "run-right";
+        if (cursors.up.isDown) return "run-up";
+        if (cursors.down.isDown) return "run-down";
+      } else {
+        if (cursors.left.isDown) return "walk-left";
+        if (cursors.right.isDown) return "walk-right";
+        if (cursors.up.isDown) return "walk-up";
+        if (cursors.down.isDown) return "walk-down";
+      }
+      return "idle";
+    },
+    onExit() {},
+  },
+  "walk-left": {
+    onEnter() {
+      foxGirl.anims.play("walk-left", true);
+    },
+    onUpdate() {
+      if (!cursors.left.isDown) return "idle";
+      foxGirl.x -= speed;
+      return "walk-left";
+    },
+    onExit() {},
+  },
+  "walk-right": {
+    onEnter() {
+      foxGirl.anims.play("walk-right", true);
+    },
+    onUpdate() {
+      if (!cursors.right.isDown) return "idle";
+      foxGirl.x += speed;
+      return "walk-right";
+    },
+    onExit() {},
+  },
+  "walk-down": {
+    onEnter() {
+      foxGirl.anims.play("walk-down", true);
+    },
+    onUpdate() {
+      if (!cursors.down.isDown) return "idle";
+      foxGirl.y += speed;
+      return "walk-down";
+    },
+    onExit() {},
+  },
+  "walk-up": {
+    onEnter() {
+      foxGirl.anims.play("walk-up", true);
+    },
+    onUpdate() {
+      if (!cursors.up.isDown) return "idle";
+      foxGirl.y -= speed;
+      return "walk-up";
+    },
+    onExit() {},
+  },
+  "run-left": {
+    onEnter() {
+      foxGirl.anims.play("walk-left", true);
+    },
+    onUpdate() {
+      if (!cursors.left.isDown || !cursors.shift.isDown) return "idle";
+      foxGirl.x -= speed + 3;
+      return "run-left";
+    },
+    onExit() {},
+  },
+  "run-right": {
+    onEnter() {
+      foxGirl.anims.play("walk-right", true);
+    },
+    onUpdate() {
+      if (!cursors.right.isDown || !cursors.shift.isDown) return "idle";
+      foxGirl.x += speed + 3;
+      return "run-right";
+    },
+    onExit() {},
+  },
+  "run-up": {
+    onEnter() {
+      foxGirl.anims.play("walk-up", true);
+    },
+    onUpdate() {
+      if (!cursors.up.isDown || !cursors.shift.isDown) return "idle";
+      foxGirl.y -= speed + 3;
+      return "run-up";
+    },
+    onExit() {},
+  },
+  "run-down": {
+    onEnter() {
+      foxGirl.anims.play("walk-down", true);
+    },
+    onUpdate() {
+      if (!cursors.down.isDown || !cursors.shift.isDown) return "idle";
+      foxGirl.y += speed + 3;
+      return "run-down";
+    },
+    onExit() {},
+  },
+};
+
 function preload() {
-  this.load.image("sky", "assets/sky.png");
-  this.load.image("pet", "assets/pet.gif");
-  this.load.image("star", "assets/star.gif");
+  this.load.spritesheet("foxGirl", "assets/foxgirl.png", {
+    frameWidth: 120,
+    frameHeight: 120,
+  });
 }
 
 function create() {
-  const width = this.sys.game.config.width;
-  const height = this.sys.game.config.height;
-
-  this.add.image(width / 2, height / 2, "sky");
-
-  player = this.add.image(400, 300, "pet");
+  foxGirl = this.add.sprite(400, 300, "foxGirl");
   cursors = this.input.keyboard.createCursorKeys();
-  this.input.on(
-    "pointerdown",
-    function (pointer) {
-      this.add.image(pointer.x, pointer.y, "star").setScale(0.5);
-    },
-    this
-  );
+
+  this.anims.create({
+    key: "walk-left",
+    frames: this.anims.generateFrameNumbers("foxGirl", { start: 18, end: 23 }),
+    frameRate: 10,
+    repeat: -1,
+  });
+
+  this.anims.create({
+    key: "walk-right",
+    frames: this.anims.generateFrameNumbers("foxGirl", { start: 12, end: 17 }),
+    frameRate: 10,
+    repeat: -1,
+  });
+
+  this.anims.create({
+    key: "walk-up",
+    frames: this.anims.generateFrameNumbers("foxGirl", { start: 6, end: 11 }),
+    frameRate: 10,
+    repeat: -1,
+  });
+
+  this.anims.create({
+    key: "walk-down",
+    frames: this.anims.generateFrameNumbers("foxGirl", { start: 0, end: 5 }),
+    frameRate: 10,
+    repeat: -1,
+  });
+
+  currentState = "idle";
+  states[currentState].onEnter();
 }
 
 function update() {
-  const speed = 3;
-  if (cursors.left.isDown) {
-    player.x -= speed;
-  } else if (cursors.right.isDown) {
-    player.x += speed;
-  }
-
-  if (cursors.up.isDown) {
-    player.y -= speed;
-  } else if (cursors.down.isDown) {
-    player.y += speed;
+  const nextState = states[currentState].onUpdate();
+  if (nextState !== currentState) {
+    states[currentState].onExit();
+    states[nextState].onEnter();
+    currentState = nextState;
   }
 }
